475002d4632f9ead9e160f4c5fa2e460481f5b59
Lilia Voller <Lilia@voller.co.uk>
2026-03-25 13:29:14 +0000

    Made it runnable from other python files

--- DataCleaner.py
+++ DataCleaner.py
@@ -3,22 +3,6 @@ import pandas
 # For matching regex of years for harvest_year and altitude ranges
 import re
 
-# Opens the CSV file
-df = pandas.read_csv('data/simplified_coffee_ratings.csv')
-
-# Drop lot_number column as more than 70% of the values are missing
-df.drop(columns=['lot_number'],inplace=True)
-
-# Get rid of duplicate columns (owner/owner_1) but make sure no gaps are left
-df['owner'] = df['owner'].fillna(df['owner_1'])
-df.drop(columns=['owner_1'],inplace=True)
-
-# Drop any rows that do not have a country, species or owner
-df.dropna(subset=["country_of_origin","species","owner"],inplace=True)
-
-# Get rid of whitespace
-df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-
 # Convert all bag_weight in pounds to kgs and remove string
 def convert_bag_weight(weight):
     if pandas.isna(weight):
@@ -38,13 +22,6 @@ def convert_bag_weight(weight):
         weight_value = int(weight_value/2.2)
     return weight_value
 
-df["bag_weight"] = df["bag_weight"].apply(convert_bag_weight)
-
-# Get rid of outliers
-quartiles = df["bag_weight"].quantile([0.25,0.75])
-outlier_bound = quartiles[1] + (quartiles[1] - quartiles[0]) * 1.5
-df.loc[df['bag_weight_kg'] > outlier_bound, 'bag_weight_kg'] = pandas.NA
-
 # Make harvest year into a singular integer year (or NA)
 def get_harvest_year(data):
     if pandas.isna():
@@ -59,7 +36,6 @@ def get_harvest_year(data):
         else:
             data = pandas.NA
     return data
-df["harvest_year"] = df["harvest_year"].apply(get_harvest_year)
 
 # Clean up altitude into a single number measured in meters
 def parse_altitudes(data):
@@ -111,17 +87,52 @@ def parse_altitudes(data):
     data = round(data)
 
     return data
-df["altitude"] = df["altitude"].apply(parse_altitudes)
 
-# List of columns with number values
-int_cols = ["number_of_bags", "bag_weight", "aroma", "flavor","aftertaste","acidity","body","balance","uniformity","clean_cup","sweetness","cupper_points","moisture"]
-# Get mean of each column and replace any empty values with it
-for col_name in int_cols:
-    mean = df[col_name].mean()
-    df.fillna({col_name: mean}, inplace=True)
+# Runs the algorithm to clean a csv file where a csv file is imported
+# This can be used on any csv file in the format of the current coffee-analysis with different data
+# To run, from DataCleaner import * then call data_cleaning_algo(<insert csv filepath here>)
+def data_cleaning_algo(csv_file_path):
+    # Opens the CSV file
+    df = pandas.read_csv(csv_file_path)
 
-# List of columns with string values
-str_cols = ["farm_name","mill","company","region","producer", "in_country_partner", "harvest_year", "grading_date", "variety", "processing_method"]
-# Replace empty values with "Unknown" for string columns
-for col_name in str_cols:
-    df.fillna({col_name: "unknown"}, inplace=True)
+    # Drop lot_number column as more than 70% of the values are missing
+    df.drop(columns=['lot_number'],inplace=True)
+
+    # Get rid of duplicate columns (owner/owner_1) but make sure no gaps are left
+    df['owner'] = df['owner'].fillna(df['owner_1'])
+    df.drop(columns=['owner_1'],inplace=True)
+
+    # Drop any rows that do not have a country, species or owner
+    df.dropna(subset=["country_of_origin","species","owner"],inplace=True)
+
+    # Get rid of whitespace
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+
+    # Converts bag_weights to kgs
+    df["bag_weight"] = df["bag_weight"].apply(convert_bag_weight)
+
+    # Get rid of outliers
+    quartiles = df["bag_weight"].quantile([0.25,0.75])
+    outlier_bound = quartiles[1] + (quartiles[1] - quartiles[0]) * 1.5
+    df.loc[df['bag_weight_kg'] > outlier_bound, 'bag_weight_kg'] = pandas.NA
+
+    # Starts cleaning harvest_year
+    df["harvest_year"] = df["harvest_year"].apply(get_harvest_year)
+
+    # Starts cleaning altitude
+    df["altitude"] = df["altitude"].apply(parse_altitudes)
+
+    # List of columns with number values
+    int_cols = ["number_of_bags", "bag_weight", "altitude", "aroma", "flavor","aftertaste","acidity","body","balance","uniformity","clean_cup","sweetness","cupper_points","moisture"]
+    # Get mean of each column and replace any empty values with it
+    for col_name in int_cols:
+        mean = df[col_name].mean()
+        df.fillna({col_name: mean}, inplace=True)
+
+    # List of columns with string values
+    str_cols = ["farm_name","mill","company","region","producer", "in_country_partner", "harvest_year", "grading_date", "variety", "processing_method"]
+    # Replace empty values with "Unknown" for string columns
+    for col_name in str_cols:
+        df.fillna({col_name: "unknown"}, inplace=True)
+
+    return df
